98a6edb337fa79772bdb3450ec9c41f4931474b5
gusadolfo123 <gusadolfo123@hotmail.com>
2018-11-15 08:15:59 -0500

    ajustes

--- app/models/course.rb
+++ app/models/course.rb
@@ -15,5 +15,8 @@ class Course < ApplicationRecord
   has_many :categories_course
   has_many :categories, through: :categories_course
   
+  # # Para relacion muchos a muchos
+  has_many :courses_student
+  has_many :students, through: :courses_student
   
 end
--- app/serializers/course_serializer.rb
+++ app/serializers/course_serializer.rb
@@ -1,5 +1,6 @@
 class CourseSerializer < ActiveModel::Serializer
-  attributes :id, :Title, :Description, :UrlImage
+  attributes :id, :Title, :Description, :UrlImage, :created_at
   # has_many :categories
   has_one :teacher
+  has_many :students
 end
